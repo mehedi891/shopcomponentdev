3590e07d179c83e0ea86fe8017b2807b89b18add
Md Mehedi Hasan <mehedihasan@Mds-MacBook-Air.local>
2025-08-31 14:53:42 +0600

    update fixed

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about [your app mm]</h1>
+        <h1 className={styles.heading}>A short heading about ShopComponent</h1>
         <p className={styles.text}>
           A tagline about [your app] that describes your value proposition.
         </p>
@@ -31,7 +31,7 @@ export default function App() {
               <span>e.g: my-shop-domain.myshopify.com</span>
             </label>
             <button className={styles.button} type="submit">
-              Log inn
+              Log in
             </button>
           </Form>
         )}
