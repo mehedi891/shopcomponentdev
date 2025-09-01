cb9f8e4fa65a8c1165a953a698a4b5b33c64039b
Md Mehedi Hasan <mehedihasan@Mds-MacBook-Air.local>
2025-09-01 07:55:07 +0600

    tawk to disabled

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -33,20 +33,20 @@ export default function App() {
   const { apiKey, shopData, TAWK_TO_URL } = useLoaderData();
   const { t } = useTranslation();
 
-  useEffect(() => {
-    if (TAWK_TO_URL) {
-      var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-      (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = TAWK_TO_URL;
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-      })();
-    }
+  // useEffect(() => {
+  //   if (TAWK_TO_URL) {
+  //     var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+  //     (function () {
+  //       var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  //       s1.async = true;
+  //       s1.src = TAWK_TO_URL;
+  //       s1.charset = 'UTF-8';
+  //       s1.setAttribute('crossorigin', '*');
+  //       s0.parentNode.insertBefore(s1, s0);
+  //     })();
+  //   }
 
-  }, []);
+  // }, []);
 
 
   // useEffect(() => {
