ef25510779e8da5abf77736290e7d85de5aabc6d
Md Mehedi Hasan <mehedihasan@Mds-MacBook-Air.local>
2025-09-26 23:49:58 +0600

    show live chat

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -34,13 +34,13 @@ export default function App() {
   const { t } = useTranslation();
 
 
-  // useEffect(() => {
-  //   (function (c, l, a, r, i, t, y) {
-  //     c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-  //     t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-  //     y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  //   })(window, document, "clarity", "script", "t2ssobpnf1");
-  // }, []);
+  useEffect(() => {
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "t2ssobpnf1");
+  }, []);
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
@@ -62,7 +62,7 @@ export default function App() {
         </div>
         <div style={{ flexFlow: 1 }}>
           <SpcFooter planName={shopData?.plan?.planName || ''} />
-          {/* <TwakTo/> */}
+          <TwakTo/>
         </div>
       </div>
 
