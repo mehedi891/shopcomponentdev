a9dcda8751763a424b4c8ba4a33a944196f634ce
Md Mehedi Hasan <mehedihasan@Mds-MacBook-Air.local>
2025-12-01 17:02:48 +0600

    chart update

--- app/routes/app.affiliate.jsx
+++ app/routes/app.affiliate.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from "@remix-run/react";
-//import { PolarisVizProvider } from '@shopify/polaris-viz';
 const Affiliate = () => {
    return (
-      // <PolarisVizProvider>
+     
          <Outlet />
-      // </PolarisVizProvider>
+  
    );
 
 }
