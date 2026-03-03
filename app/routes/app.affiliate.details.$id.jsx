2fb4610bdfb1da11ebd7b32756bd05301b56c10f
Md Mehedi Hasan <mehedihasan@Mds-MacBook-Air-2.local>
2026-03-03 14:19:54 +0600

    remove useState

--- app/routes/app.affiliate.details.$id.jsx
+++ app/routes/app.affiliate.details.$id.jsx
@@ -3,7 +3,7 @@ import db from "../db.server";
 import { authenticate } from "../shopify.server";
 import LoadingSkeleton from "../components/LoadingSkeleton/LoadingSkeleton";
 import { AFFILIATE_STATUS, COMISSION_CRITERIA } from "../constants/constants";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import EmptyStateGeneric from "../components/EmptyStateGeneric/EmptyStateGeneric";
 import { capitalizeFirstCaracter } from "../utilis/generalUtils";
 import TransactionModal from "../components/TransactionModal/TransactionModal";
